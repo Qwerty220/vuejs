c990ec6ebf7515aec3823f061aa07eac4ea91f1c
Qwerty220 <qwerty220@xakep.ru>
2018-09-18 15:37:01 +0300

    add elements

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,52 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+	  <el-table
+		:data="tableData"
+		border
+		style="width: 100%">
+		<el-table-column
+		  prop="date"
+		  label="Date"
+		  width="180">
+		</el-table-column>
+		<el-table-column
+		  prop="name"
+		  label="Name"
+		  width="180">
+		</el-table-column>
+		<el-table-column
+		  prop="address"
+		  label="Address">
+		</el-table-column>
+	  </el-table>
   </div>
 </template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from "vue-property-decorator";
+
+    @Component
+    export default class App extends Vue {
+
+	tableData = [{
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }, {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles'
+        }];
+}
+</script>
 
 <style>
 #app {
@@ -15,17 +55,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
